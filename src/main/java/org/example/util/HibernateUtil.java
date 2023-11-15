2d0c37b919957c21a998cfc42554854d64ddb344
shaon <shaonx@proton.me>
2023-11-15 20:40:44 +0600

    Update closeSessionFactory() method in class HibernateUtil

--- src/main/java/org/example/util/HibernateUtil.java
+++ src/main/java/org/example/util/HibernateUtil.java
@@ -6,13 +6,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static final SessionFactory sessionFactory;
+    private static final SessionFactory SESSION_FACTORY;
 
     private HibernateUtil() {
     }
 
     static {
-        sessionFactory = buildSessionFactory();
+        SESSION_FACTORY = buildSessionFactory();
     }
 
     private static SessionFactory buildSessionFactory() {
@@ -38,10 +38,12 @@ public class HibernateUtil {
     }
 
     public static SessionFactory getSessionFactory() {
-        return sessionFactory;
+        return SESSION_FACTORY;
     }
 
     public static void closeSessionFactory() {
-        sessionFactory.close();
+        if (SESSION_FACTORY.isOpen()) {
+            SESSION_FACTORY.close();
+        }
     }
 }
